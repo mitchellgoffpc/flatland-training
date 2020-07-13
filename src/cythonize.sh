d284c57a01d250a8eea8f78ab73c90f4fc633121
Luke <39779310+ClashLuke@users.noreply.github.com>
2020-07-13 11:30:11 +0200

    feat(rail-generator): cythonize

--- src/cythonize.sh
+++ src/cythonize.sh
@@ -1,7 +1,7 @@
 function compile {
   file=${1}
   cython "$file.pyx"  -3 -Wextra -D
-  cmd="gcc-7 $file.c `python3-config --cflags --ldflags --includes --libs` -fno-lto -pthread -fPIC -fwrapv -pipe -march=native -mtune=native -Ofast -msse2 -msse4.2 -shared -o $file.so"
+  cmd="gcc-7 $file.c `python3-config --cflags --ldflags --includes --libs` -I`python -c 'import numpy, sys; sys.stdout.write(numpy.get_include()); sys.stdout.flush()'` -fno-lto -pthread -fPIC -fwrapv -pipe -march=native -mtune=native -Ofast -msse2 -msse4.2 -shared -o $file.so"
   echo "Executing $cmd"
   $cmd
   echo "Testing compilation.."
@@ -11,3 +11,4 @@ function compile {
 
 compile observation_utils
 compile rail_env
+compile rail_generators
--- src/generate_railways.py
+++ src/generate_railways.py
@@ -14,23 +14,17 @@ except:
 project_root = Path(__file__).resolve().parent.parent
 parser = argparse.ArgumentParser(description="Train an agent in the flatland environment")
 
-parser.add_argument("--agents", type=int, default=3, help="Number of episodes to train for")
-parser.add_argument("--cities", type=int, default=3, help="Number of episodes to train for")
 parser.add_argument("--width", type=int, default=35, help="Decay factor for epsilon-greedy exploration")
-parser.add_argument("--height", type=int, default=None, help="Decay factor for epsilon-greedy exploration")
-
 flags = parser.parse_args()
 
 width = flags.width
-height = width if flags.height is None else flags.height
-n_agents = flags.agents
-rail_generator, schedule_generator = create_random_railways(project_root, flags.cities)
+rail_generator, schedule_generator = create_random_railways(flags.width)
 
 # Load in any existing railways for this map size so we don't overwrite them
 try:
-    with open(project_root / f'railroads/rail_networks_{n_agents}x{width}x{height}.pkl', 'rb') as file:
+    with open(project_root / f'railroads/rail_networks_{width}.pkl', 'rb') as file:
         rail_networks = pickle.load(file)
-    with open(project_root / f'railroads/schedules_{n_agents}x{width}x{height}.pkl', 'rb') as file:
+    with open(project_root / f'railroads/schedules_{width}.pkl', 'rb') as file:
         schedules = pickle.load(file)
     print(f"Loading {len(rail_networks)} railways...")
 except:
@@ -39,8 +33,8 @@ except:
 
 def do(schedules: list, rail_networks: list):
     for _ in range(100):
-        map, info = rail_generator(width, height, n_agents, num_resets=0, np_random=np.random)
-        schedule = schedule_generator(map, n_agents, info['agents_hints'], num_resets=0, np_random=np.random)
+        map, info = rail_generator(width, 1, 1, num_resets=0, np_random=np.random)
+        schedule = schedule_generator(map, 1, info['agents_hints'], num_resets=0, np_random=np.random)
         rail_networks.append((map, info))
         schedules.append(schedule)
     return
@@ -52,9 +46,9 @@ shared_rail_networks = manager.list(rail_networks)
 # Generate 10000 random railways in 100 batches of 100
 for _ in tqdm(range(500), ncols=150, leave=False):
     do(schedules, rail_networks)
-    with open(project_root / f'railroads/rail_networks_{n_agents}x{width}x{height}.pkl', 'wb') as file:
+    with open(project_root / f'railroads/rail_networks_{width}.pkl', 'wb') as file:
         pickle.dump(schedules, file, protocol=4)
-    with open(project_root / f'railroads/schedules_{n_agents}x{width}x{height}.pkl', 'wb') as file:
+    with open(project_root / f'railroads/schedules_{width}.pkl', 'wb') as file:
         pickle.dump(rail_networks, file, protocol=4)
 
 print(f"Saved {len(shared_rail_networks)} railways")
