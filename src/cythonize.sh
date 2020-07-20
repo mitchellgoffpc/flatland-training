3759e7713a37784aab2e531f9bee5c3defa9c762
Luke <39779310+ClashLuke@users.noreply.github.com>
2020-07-20 20:48:24 +0200

    fix(cythonize): use gcc9 instead of 7 (9 doens't work with cuda10)

--- src/cythonize.sh
+++ src/cythonize.sh
@@ -1,7 +1,7 @@
 function compile {
   file=${1}
   cython "$file.pyx"  -3 -Wextra -D
-  cmd="gcc-7 $file.c `python3-config --cflags --ldflags --includes --libs` -I`python -c 'import numpy, sys; sys.stdout.write(numpy.get_include()); sys.stdout.flush()'` -fno-lto -pthread -fPIC -fwrapv -pipe -march=native -mtune=native -Ofast -msse2 -msse4.2 -shared -o $file.so"
+  cmd="gcc-9 $file.c `python3-config --cflags --ldflags --includes --libs` -I`python -c 'import numpy, sys; sys.stdout.write(numpy.get_include()); sys.stdout.flush()'` -fno-lto -pthread -fPIC -fwrapv -pipe -march=native -mtune=native -Ofast -msse2 -msse4.2 -shared -o $file.so"
   echo "Executing $cmd"
   $cmd
   echo "Testing compilation.."
